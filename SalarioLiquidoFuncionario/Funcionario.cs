18fcda5e42a87282df42d3305d93779f0243963d
Matheus Vieira <matheusvr423@gmail.com>
2021-04-03 23:08:36 -0300

    Atividade propondo o calculo do salario liquido e o aumento do salario de um funcionario

---
+++ SalarioLiquidoFuncionario/Funcionario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+namespace SalarioLiquidoFuncionario
+{
+    class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto, Imposto;
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            SalarioBruto += (SalarioBruto * porcentagem / 100.0);
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", $ "
+                + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+                
+        }
+
+    }
+}
---
+++ SalarioLiquidoFuncionario/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace SalarioLiquidoFuncionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Funcionario f = new Funcionario();
+            
+            
+            Console.Write("Nome: ");
+            f.Nome = Console.ReadLine();
+
+            Console.Write("Salário bruto: ");
+            f.SalarioBruto = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.Write("Imposto: ");
+            f.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Funcionário: " +f);
+            Console.WriteLine();
+
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            double  porcent = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            
+            f.AumentarSalario(porcent);
+
+            Console.Write("Dados atualizados: " +f);
+
+        }
+    }
+}
